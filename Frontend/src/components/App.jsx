31db544c472d968087b3879021c79d2bb8af9a8c
Karol Wiśniewski <karolwisniewski92@gmail.com>
2021-04-18 16:20:39 +0200

    Add component App. App will be temporary use to check connection with backend

--- Frontend/src/components/App.jsx
+++ Frontend/src/components/App.jsx
@@ -1,9 +1,41 @@
 import React from 'react';
+import settings from '../settings'
+
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            infoMessage: ""
+        }
+    }
+
+    checkConnection = () => {
+        // this.setState({
+        //     ...this.state,
+        //     infoMessage: ""
+        // })
+
+    }
+
     render() {
         return (
-            <div>To jest pierwszy komponent!</div>
+            <div className="app__wrapper">
+                <i className="fas fa-rocket app_rocket-icon"></i>
+                <h1
+                    className="app_main-text">
+                    Nasz pierwszy projekt!
+                </h1>
+                <button
+                    className="app_check-btn"
+                    onClick={this.checkConnection}>
+                    Kliknij aby sprawdzić połączenie z backend!
+                </button>
+                <div
+                    className="app_info-message">
+                    {this.state.infoMessage}
+                </div>
+            </div>
         )
     }
 }
