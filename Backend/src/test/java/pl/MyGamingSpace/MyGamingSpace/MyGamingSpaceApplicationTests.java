24b1dbf5aa0e426e2c84e9625bb7c36d1597ba23
dlukaszewski <RTX4.Cm8jnr4d>
2021-04-23 20:11:44 +0200

    third commit

---
+++ Backend/src/test/java/pl/MyGamingSpace/MyGamingSpace/MyGamingSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.MyGamingSpace.MyGamingSpace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyGamingSpaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
